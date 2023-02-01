ee4c73593701621ab22c3678a33c6a487b12e488
Alex Komissarchik <110857142+raspyrandom@users.noreply.github.com>
2023-02-01 18:20:52 +0000

    made into a node project

--- replit.nix
+++ replit.nix
@@ -1,7 +1,8 @@
 { pkgs }: {
-  deps = [
-    pkgs.nodejs-16_x
-    pkgs.nodePackages.vscode-langservers-extracted
+	deps = [
+		pkgs.nodejs-18_x
     pkgs.nodePackages.typescript-language-server
-  ];
+    pkgs.yarn
+    pkgs.replitPackages.jest
+	];
 }
