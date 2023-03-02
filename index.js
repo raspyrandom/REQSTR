5da503b48873871f5f730e36725f4e9e08e6de97
Alex Komissarchik <110857142+raspyrandom@users.noreply.github.com>
2023-03-02 16:26:37 +0000

    this will proabably work

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ const storage = multer.diskStorage({
 
 app.use("/REQSTR/views/view-posts",(req, res)=> {
   fileLoop();
-  res.sendFile("views/imageView.html");
+  res.sendFile("https://replit.com/@AlexKomissarch1/REQSTR-server#views/imageView.html");
 });
 
 // storage thing necessary for multer, doesn't need to be changed
